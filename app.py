1af19cf14906d869fafdaa9da6d5d4e94966331a
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-05-23 17:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.write("TEST")
-video_file = open("https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4")
+video_file = open("/main/video/test.mp4")
+#https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4
 video_bytes = video_file.read()
 st.video(video_bytes)
