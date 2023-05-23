4aa69245a23c682b0ee637c607a4901ee5faf0c9
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-05-23 18:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ st.write("TEST")
 #video_bytes = video_file.read()
 
 with st.container():
-    col1,col2,col3= st.columns([3,1,2])
+    col1,col2,col3,col4= st.columns([3,1,1,1])
     with col1:
       st.video("https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4?raw=true")
-    with col2:
-      st.image("https://github.com/JVJayarah3/about_me/blob/main/video/drillstring.JPG?raw=true")
     with col3:
+      st.image("https://github.com/JVJayarah3/about_me/blob/main/video/drillstring.JPG?raw=true")
+    with col4:
       b1 = st.button("ABOUT ME")
       st.empty()
       b2 = st.button("STRENGTH & WEAKNESS")
