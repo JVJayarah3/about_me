1f40574a0d5cbeb5f52defc047a93e91ee2aa2f5
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-05-23 17:58:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.write("TEST")
 #video_file = open("/main/video/test.mp4")
 #https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4
 #video_bytes = video_file.read()
-st.video("https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4")
+st.video("https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4?raw=true"")
