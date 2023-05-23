02a4fd9c7d3a0e31c7e4caf64bdff479c2d12c42
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-05-23 18:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-
 st.write("TEST")
 #video_file = open("/main/video/test.mp4")
 #https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4
 #video_bytes = video_file.read()
 st.video("https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4?raw=true")
+st.image("https://github.com/JVJayarah3/about_me/blob/main/video/drillstring.JPG?raw=true")
+b1 = st.button("ABOUT ME")
+b2 = st.button("STRENGTH & WEAKNESS")
+b3 = st.button("")
+b4 = st.button("")
