dc2e8a692b6c3766a3d9fb6efa9bcbd8badeef04
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-05-23 18:20:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.write("TEST")
 #video_bytes = video_file.read()
 
 with st.container():
-    col1,col2,col3,col4 = st.columns([7,1.5,2,1])
+    col1,col2,col3= st.columns([5,1,2])
     with col1:
       st.video("https://github.com/JVJayarah3/about_me/blob/main/video/test.mp4?raw=true")
     with col2:
