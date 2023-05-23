c250c54ce17671fd4c01bcb64f339a5a711f1da5
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-05-23 17:53:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.write("TEST")
-video_file = open("https://github.com/JVJayarah3/about_me/blob/main/video/Blue Grey Modern User Persona Graph (1).mp4?raw=true")
+video_file = open("https://github.com/JVJayarah3/about_me/blob/main/video/Blue Grey Modern User Persona Graph (1).mp4")
 video_bytes = video_file.read()
 st.video(video_bytes)
